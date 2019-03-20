b0914e47f89dbd9d120658288183a510965ebd39
Davy Tenn <davy.lsdev@gmail.com>
2019-03-19 21:34:04 -0600

    Improve meter
    * improve the meter line calculation
    * use structures to define meter face parameters * rather than hard-coding them
    Broken: the needle angle isn't correct most of the time, so the calculation still needs some tweaking.

--- Testing/lcdMeter/lcdMeter.ino
+++ Testing/lcdMeter/lcdMeter.ino
@@ -13,44 +13,14 @@
 // Arduino pin 7 - LCD chip enable (SCE)
 Adafruit_PCD8544 display = Adafruit_PCD8544(3, 4, 5, 7, 6);
 
-// Needed function
-float mInv(float divider){
-  float inverse = 1.0/divider;
-  return inverse;
-}
-
-const int solarPanelInput = A0;
-const int isoPot = A1;
-const int timePot = A2;
-const int aperaturePot = A3;
-int isoValueTable[] = {-375,-75,15,3,6,12, \
-   25,50,100,200,400,800,1600,3200};   
-const int timeValueTable[] = {-4000,-2000,-1000, \
-  -500,-250,-125,-60,-30,-15,-8,-4,-2, \
-  1,2,4,8,15,30,60,120,240,3600,7200,14400,28800};
-const int aperatureValueTable[] = { 1,2,3,4,6,8, \
-  11,22,32,45,64,90,128,181,256,362,512,724,1024};
-int solarPanelValue = 0;
-float isoValue = 0;
-float timeValue = 0;
-float aperatureValue = 0;
 int lcdContrast = 50;
-int lastSolarPanelValue = -1;
-
-String text = "LUX: ";
-String text2 = "ISO: ";
-
-//int potValue = 0;
 
 void setup()                    
 {
   Serial.begin(9600);
   
-  display.begin();
-  // init done
-
-  // you can change the contrast around to adapt the display
-  // for the best viewing!
+  display.begin(); // initialize display
+  
   display.setContrast(lcdContrast);
   display.setTextSize(1);
   display.setRotation(2);
@@ -61,29 +31,13 @@ void loop()
 {
   display.display();
 //  test();
-  drawMeter("f4.5", "1/1000", 3200 );
+//  drawMeter("f4.5", "1/1000", 3200 );
   for (int i = 0; i < 101; i++){
+    drawMeter("f4.5", "1/1000", 3200 );
     updateMeter (i);
+    display.clearDisplay();
     delay(300);
   }
-  
-}
-
-int getSolarPanelReading(){
-// Tweak these next values for light reading  
-  const int minLight = 0;
-  const int maxLight = 360;
-  int lightReading = analogRead(solarPanelInput);
-  int lightRange = map (lightReading, minLight, maxLight, 0, 7);
-  return lightRange; 
-}
-
-int getPotValue(int potIdentity, int potMinValue, int potMaxValue, int potValueTable[]){
-  int value = analogRead(potIdentity);
-//  int arraySize = sizeof(potValueTable) - 1;
-  int potRange = map (value, potMinValue, potMaxValue, 0, 13);
-  int valueFromTable = potValueTable[potRange];
-  return valueFromTable;
 }
 
 void updateInformation (int lightIntensity, int iso, int shutterTime, int aperature ){
@@ -142,65 +96,80 @@ void test(){
 }
 
 void drawMeter(String fstop, String shutter, int iso){
+
+  int numberOfScaleMarks = 5;
+  int scaleMarksAlignment = 1; // 0 -> top, 1 -> centre, 2 -> bottom
+  int scaleLineHeight = 4;
+  int scaleRadius = 20;
+  int needleBaseFillWidth = 2;
+  int bracketLength = 34;
+  int bracketHeight = 8;
+
+  String minusSign = "-";
+  String plusSign = "+";
+   
+  struct coordinate {
+    int x;
+    int y;
+  };
+
+  struct coordinate fstopCoordinate = {0, 0};
+  struct coordinate isoLableCoordinate = {0, 40};
+  struct coordinate isoValueCoordinate = {60, 40};
+  struct coordinate shutterSpeedCoordinate = {48, 0};
+  struct coordinate leftBracketCoordinate = {0, 38};
+  struct coordinate rightBracketCoordinate = {50, 38};
+  struct coordinate needleBaseCoordinate = {43, 37};
+  struct coordinate minusSignCoordinate = {28, 40};
+  struct coordinate plusSignCoordinate = {52, 40};
+  
   display.clearDisplay();
   
 // lables
-  display.setCursor(0, 0);
+  display.setCursor(fstopCoordinate.x, fstopCoordinate.y);
   display.println(fstop);
-  display.setCursor(48, 0);
+  display.setCursor(shutterSpeedCoordinate.x, shutterSpeedCoordinate.y);
   display.println(shutter);
-  display.setCursor(0, 40);
+  display.setCursor(isoLableCoordinate.x, isoLableCoordinate.y);
   display.println("ISO");
-  display.setCursor(60, 40);
+  display.setCursor(isoValueCoordinate.x, isoValueCoordinate.y);
   display.println(String (iso, DEC));
-  display.setCursor(28, 40);
-  display.println("-");
-  display.setCursor(52, 40);
-  display.println("+");
+  display.setCursor(minusSignCoordinate.x, minusSignCoordinate.y);
+  display.println(minusSign);
+  display.setCursor(plusSignCoordinate.x, plusSignCoordinate.y);
+  display.println(plusSign);
 
-// define the meter bottom
-  display.drawFastHLine(0, 38, 34, BLACK);
-  display.drawFastVLine(34, 38, 8, BLACK);
-  display.drawFastHLine(50, 38, 34, BLACK);
-  display.drawFastVLine(50, 38, 8, BLACK);
+// draw the meter bottom brackets
+  display.drawFastHLine(leftBracketCoordinate.x, leftBracketCoordinate.y, bracketLength, BLACK);
+  display.drawFastVLine(leftBracketCoordinate.x+bracketLength, leftBracketCoordinate.y, bracketHeight, BLACK);
+  display.drawFastHLine(rightBracketCoordinate.x, rightBracketCoordinate.y, bracketLength, BLACK);
+  display.drawFastVLine(rightBracketCoordinate.x, rightBracketCoordinate.y, bracketHeight, BLACK);
   
-// the centre of the needle  
-  display.fillCircle( 43, 37, 2, BLACK);
+// draw the centre of the needle  
+  display.fillCircle( needleBaseCoordinate.x, needleBaseCoordinate.y, needleBaseFillWidth, BLACK);
   display.display();
 }
 
 void updateMeter (int meterValue){
   int xInit = 43;
   int yInit = 37;
-  int radius = 35;
+  int radius = 29;
 
-  int x;
-  int y;
+  int xTip;
+  int yTip;
 
   int angle = 1.8*meterValue;
-  if (meterValue >90){
-    x = abs(sin(angle))*radius;
-  } else {
-    x = abs(sin(angle))*radius;
-  }
-  
-  y = abs(cos(angle))*radius;
-
-  display.drawLine(xInit, yInit, x, y, BLACK);
-  
-//  switch (meterValue) {
-//    case 1:
-//      display.drawLine(xInit, yInit, 0, 37, BLACK);
-//      break;
-//    case 2:
-//      display.drawLine(xInit, yInit, 43, 9, BLACK);
-//      break;
-//    case 3:
-//      display.drawLine(xInit, yInit, 84, 37, BLACK);
-//      break;
-//    default:
-//      display.drawLine(xInit, yInit, 43, 9, BLACK);;
-//      break;
+//  if (meterValue >90){
+//    xTip = abs(sin(angle))*radius;
+//  } else {
+//    x = abs(sin(angle))*radius;
 //  }
+//  
+//  y = abs(cos(angle))*radius;
+
+  xTip = xInit + radius*cos(angle);
+  yTip = yInit - radius*abs(sin(angle));
+
+  display.drawLine(xInit, yInit, xTip, yTip, BLACK);
   display.display();
 }
